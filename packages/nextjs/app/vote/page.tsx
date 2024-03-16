515f62603364e46cb127e40750fbed2781d4ef2d
Yash Goyal <yashgo0018@gmail.com>
2024-03-16 18:31:10 +0000

    partial commit

--- packages/nextjs/app/vote/page.tsx
+++ packages/nextjs/app/vote/page.tsx
@@ -3,9 +3,27 @@
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+// import { PubKey } from "@se-2/hardhat/domainobjs";
+import { useContractRead } from "wagmi";
+import PollAbi from "~~/abi/Poll.abi";
 import HoverBorderCard from "~~/components/card/HoverBorderCard";
 import VoteCard from "~~/components/card/VoteCard";
 import LoaderPage from "~~/components/loader/loader";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+
+/**
+ *
+ * KP 1
+ * [✓] Public key: macipk.31c3b9d36aed8ab9490d7468cb581669ff2774791c7fcefc4cdd07bd009a019d
+ * [✓] Private key: macisk.79a8acc8572683cd2f6a212b7ac096f66b39dbad6ceb11634a1b0d7584df4856
+ *
+ * KP 2
+ * [✓] Public key: macipk.f48bc9877d99ba9482ababaa53f31d492d84bf105e0c8b3fc60edbf52ed7f0a3
+ * [✓] Private key: macisk.578e007d0eb7625624c00e898379bc2b3e96af59dffa7212eb2a5ae100f24c73
+ *
+ */
+
+// const coordinatorPubKey = PubKey.deserialize("31c3b9d36aed8ab9490d7468cb581669ff2774791c7fcefc4cdd07bd009a019d");
 
 const Vote = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -27,6 +45,43 @@ const Vote = () => {
       router.push(`/voted-success?id=${clickedIndex}`);
     }, 4000);
   };
+
+  // const [pollContractAddress, setPollContractAddress] = useState<Address>();
+  const pollId = 0n;
+
+  const { data: pollContractAddress } = useScaffoldContractRead({
+    contractName: "MACI",
+    functionName: "getPoll",
+    args: [pollId],
+  });
+
+  console.log(pollContractAddress);
+
+  const { data: maxValues } = useContractRead({
+    abi: PollAbi,
+    address: pollContractAddress,
+    functionName: "maxValues",
+    args: [],
+  });
+
+  const { data: coordinatorPubKeyResult } = useContractRead({
+    abi: PollAbi,
+    address: pollContractAddress,
+    functionName: "coordinatorPubKey",
+    args: [],
+  });
+
+  // const {} = useContractWrite({
+  //   abi: PollAbi,
+  //   address: pollContractAddress,
+  //   functionName: "publishMessage",
+  //   args: [message.asContractParam(), encKeypair.pubKey.asContractParam()]
+  // })
+
+  // const castVoteImpl = () => {};
+
+  console.log(maxValues);
+  console.log(coordinatorPubKeyResult);
 
   const candidates = ["Candidate 1", "Candidate 2", "Candidate 3"];
 
