7fa699e97738a67379f86e0e63a921794b5727d6
Manu <manu1998kj@gmail.com>
2024-03-16 21:25:44 +0000

    added registeration

--- packages/nextjs/app/register/page.tsx
+++ packages/nextjs/app/register/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { Keypair, PubKey } from "@se-2/hardhat/domainobjs";
 import { IDKitWidget, ISuccessResult } from "@worldcoin/idkit";
+import Lottie from "lottie-react";
 import { encodeAbiParameters, parseAbiParameters } from "viem";
-import { useAccount } from "wagmi";
+import { useAccount, useConnect } from "wagmi";
+import walletAnimation from "~~/components/assets/wallet.json";
+import walletConnectedAnimation from "~~/components/assets/wallet_connected.json";
+import worldCoinGif from "~~/components/assets/worldcoin.gif";
+import HoverBorderCard from "~~/components/card/HoverBorderCard";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 import { decode } from "~~/lib/wld";
+import useUserRegisteredStore from "~~/services/store/user_registered_store";
 import { fetchOrCreateUserKeyPair } from "~~/utils/crypto";
 
 export default function RegisterPage() {
+  const router = useRouter();
   const { address } = useAccount();
   const [keypair, setKeyPair] = useState<Keypair | null>(null);
   const [proof, setProof] = useState<ISuccessResult | null>(null);
   const [encodedProof, setEncodedProof] = useState<`0x${string}` | undefined>();
+  const { setIsRegistered } = useUserRegisteredStore();
 
   useEffect(() => {
     setKeyPair(fetchOrCreateUserKeyPair(address));
@@ -53,6 +63,8 @@ export default function RegisterPage() {
     return { x: BigInt(p.x), y: BigInt(p.y) };
   }
 
+  const { connect, connectors } = useConnect();
+
   const { writeAsync } = useScaffoldContractWrite({
     contractName: "MACI",
     functionName: "signUp",
@@ -65,22 +77,49 @@ export default function RegisterPage() {
 
   useEffect(() => {
     writeAsync();
+    //TODO ; add a way to make it rgisteration true
+    setIsRegistered(true);
+    router.push("/home");
   }, [encodedProof]);
 
+  const style = {
+    height: 120,
+    width: 120,
+  };
+
   return (
-    <main>
-      {address ? (
+    <div className="flex flex-col bg-gradient-to-r from-[#181436] to-[#19244F] h-screen p-7">
+      <div>
+        <div className="mb-10">
+          <HoverBorderCard disabled={address != null} click={() => connect({ connector: connectors[0] })}>
+            <div className="flex justify-center w-full text-xl flex-col items-center">
+              <div className="my-2">{address ? "Wallet Connected" : "Connect to"}</div>
+              {address ? (
+                <Lottie animationData={walletConnectedAnimation} loop={false} style={style} />
+              ) : (
+                <Lottie animationData={walletAnimation} style={style} />
+              )}
+            </div>
+          </HoverBorderCard>
+        </div>
         <IDKitWidget
           signal={address}
           action="register"
           onSuccess={setProof}
           app_id={process.env.NEXT_PUBLIC_APP_ID! as `app_${string}`}
         >
-          {({ open }) => <button onClick={open}>verify with world id</button>}
+          {({ open }) => (
+            <div>
+              <HoverBorderCard click={open} disabled={address == null}>
+                <div className="flex justify-center w-full text-xl flex-col items-center ">
+                  Register with
+                  <Image className="rounded-full my-10" src={worldCoinGif} alt="my gif" height={100} width={100} />
+                </div>
+              </HoverBorderCard>
+            </div>
+          )}
         </IDKitWidget>
-      ) : (
-        <div>Connect First</div>
-      )}
-    </main>
+      </div>
+    </div>
   );
 }
