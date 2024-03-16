58654cbacf6b559e163a0a29cacf931e1ddf6ce8
Yash Goyal <yashgo0018@gmail.com>
2024-03-16 14:26:03 +0000

    started integration

--- packages/nextjs/components/poll/PollComponent.tsx
+++ packages/nextjs/components/poll/PollComponent.tsx
@@ -1,8 +1,31 @@
 import React, { useState } from "react";
 import HoverBorderCard from "../card/HoverBorderCard";
+import { PubKey } from "@se-2/hardhat/domainobjs";
 import { LuCross } from "react-icons/lu";
 import { MdEdit } from "react-icons/md";
 import { RxCross2 } from "react-icons/rx";
+import { useChainId } from "wagmi";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { contracts } from "~~/utils/scaffold-eth/contract";
+
+/**
+ *
+ * KP 1
+ * [✓] Public key: macipk.31c3b9d36aed8ab9490d7468cb581669ff2774791c7fcefc4cdd07bd009a019d
+ * [✓] Private key: macisk.79a8acc8572683cd2f6a212b7ac096f66b39dbad6ceb11634a1b0d7584df4856
+ *
+ * KP 2
+ * [✓] Public key: macipk.f48bc9877d99ba9482ababaa53f31d492d84bf105e0c8b3fc60edbf52ed7f0a3
+ * [✓] Private key: macisk.578e007d0eb7625624c00e898379bc2b3e96af59dffa7212eb2a5ae100f24c73
+ *
+ */
+
+const coordinatorPubKey = PubKey.deserialize("31c3b9d36aed8ab9490d7468cb581669ff2774791c7fcefc4cdd07bd009a019d");
+
+function keyToParam(key: PubKey): { x: bigint; y: bigint } {
+  const p = key.asContractParam();
+  return { x: BigInt(p.x), y: BigInt(p.y) };
+}
 
 interface PollData {
   title: string;
@@ -12,6 +35,7 @@ interface PollData {
 const PollComponent: React.FC = () => {
   const [pollData, setPollData] = useState<PollData>({ title: "DummyTitle", options: [""] });
   const [isEditingTitle, setIsEditingTitle] = useState<boolean>(false);
+  const chainId = useChainId();
 
   const handleAddOption = () => {
     setPollData({ ...pollData, options: [...pollData.options, ""] });
@@ -33,6 +57,33 @@ const PollComponent: React.FC = () => {
 
   const handleSaveTitleClick = () => {
     setIsEditingTitle(false);
+  };
+
+  const { write, data } = useScaffoldContractWrite({
+    contractName: "MACI",
+    functionName: "deployPoll",
+    args: [
+      90000n,
+      {
+        intStateTreeDepth: 1,
+        messageTreeSubDepth: 1,
+        messageTreeDepth: 2,
+        voteOptionTreeDepth: 2,
+      },
+      keyToParam(coordinatorPubKey),
+      (contracts && contracts[chainId] && contracts[chainId]["Verifier"]?.address) || undefined,
+      (contracts && contracts[chainId] && contracts[chainId]["VkRegistry"]?.address) || undefined,
+      false,
+    ],
+  });
+
+  console.log(chainId);
+  console.log(data);
+
+  const createPoll = () => {
+    console.log(pollData);
+
+    write();
   };
 
   return (
@@ -92,7 +143,10 @@ const PollComponent: React.FC = () => {
 
       <div className="mt-5">
         <HoverBorderCard>
-          <div className="flex justify-center w-full text-xl "> Create a Poll </div>
+          <div onClick={createPoll} className="flex justify-center w-full text-xl ">
+            {" "}
+            Create a Poll{" "}
+          </div>
         </HoverBorderCard>
       </div>
     </div>
