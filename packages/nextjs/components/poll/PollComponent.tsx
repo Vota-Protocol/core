ebcc333063a812971e1fdc4a2710572781fb903f
Manu <manu1998kj@gmail.com>
2024-03-16 14:39:25 +0000

    added remove options

--- packages/nextjs/components/poll/PollComponent.tsx
+++ packages/nextjs/components/poll/PollComponent.tsx
@@ -124,7 +124,7 @@ const PollComponent: React.FC<{ onPollDataChange: (data: PollData) => void }> = 
             </button>
           ) : (
             <button className="btn btn-outline ml-4" onClick={() => removeOptions(index)}>
-              <RxCross2 size={20} />
+              <LuCross size={20} />
             </button>
           )}
         </div>
