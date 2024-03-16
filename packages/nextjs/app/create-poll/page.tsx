d35a804af288f117d0bdcfa8748e0bd4f65402ee
Manu <manu1998kj@gmail.com>
2024-03-16 17:13:16 +0000

    used polldatamodel and moved write to page from component

--- packages/nextjs/app/create-poll/page.tsx
+++ packages/nextjs/app/create-poll/page.tsx
@@ -2,10 +2,34 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { PubKey } from "@se-2/hardhat/domainobjs";
 import Lottie from "lottie-react";
+import { useChainId } from "wagmi";
 import person from "~~/components/assets/person.json";
 import LoaderPage from "~~/components/loader/loader";
-import PollComponent, { PollData } from "~~/components/poll/PollComponent";
+import PollComponent from "~~/components/poll/PollComponent";
+import { PollData } from "~~/components/poll/PollDataModel";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { contracts } from "~~/utils/scaffold-eth/contract";
+
+/**
+ *
+ * KP 1
+ * [✓] Public key: macipk.31c3b9d36aed8ab9490d7468cb581669ff2774791c7fcefc4cdd07bd009a019d
+ * [✓] Private key: macisk.79a8acc8572683cd2f6a212b7ac096f66b39dbad6ceb11634a1b0d7584df4856
+ *
+ * KP 2
+ * [✓] Public key: macipk.f48bc9877d99ba9482ababaa53f31d492d84bf105e0c8b3fc60edbf52ed7f0a3
+ * [✓] Private key: macisk.578e007d0eb7625624c00e898379bc2b3e96af59dffa7212eb2a5ae100f24c73
+ *
+ */
+
+const coordinatorPubKey = PubKey.deserialize("31c3b9d36aed8ab9490d7468cb581669ff2774791c7fcefc4cdd07bd009a019d");
+
+function keyToParam(key: PubKey): { x: bigint; y: bigint } {
+  const p = key.asContractParam();
+  return { x: BigInt(p.x), y: BigInt(p.y) };
+}
 
 const CreateVote = () => {
   const router = useRouter();
@@ -14,11 +38,38 @@ const CreateVote = () => {
     height: 600,
     width: 600,
   };
+
+  const chainId = useChainId();
+
+  const { write, data } = useScaffoldContractWrite({
+    contractName: "MACI",
+    functionName: "deployPoll",
+    args: [
+      90000n,
+      {
+        intStateTreeDepth: 1,
+        messageTreeSubDepth: 1,
+        messageTreeDepth: 2,
+        voteOptionTreeDepth: 2,
+      },
+      keyToParam(coordinatorPubKey),
+      (contracts && contracts[chainId] && contracts[chainId]["Verifier"]?.address) || undefined,
+      (contracts && contracts[chainId] && contracts[chainId]["VkRegistry"]?.address) || undefined,
+      false,
+    ],
+  });
+
+  console.log(chainId);
+  console.log(data);
+
   function createPoll(data: PollData): void {
-    console.log(data);
-    console.log(data.country?.title);
-    console.log(data.country?.value);
     setIsLoading(true);
+    // crreating the poll
+    try {
+      write();
+    } catch (e) {
+      console.error(e);
+    }
     setTimeout(() => {
       router.push(`/create-poll-success?pollName=${data.title}`);
     }, 2000);
--- packages/nextjs/app/home/page.tsx
+++ packages/nextjs/app/home/page.tsx
@@ -4,38 +4,16 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import HoverBorderCard from "~~/components/card/HoverBorderCard";
 import LoaderPage from "~~/components/loader/loader";
-
-interface Voter {
-  id: number;
-  title: string;
-  numberOfCandidates: number;
-}
+import { PollData, listOfMockPolls } from "~~/components/poll/PollDataModel";
 
 const VoterPage = () => {
   const router = useRouter();
-  const [voters, setVoters] = useState<Voter[] | null>(null);
+  const [voters, setVoters] = useState<PollData[] | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      const data = [
-        {
-          id: 1,
-          title: "Election 1",
-          numberOfCandidates: 3,
-        },
-        {
-          id: 2,
-          title: "Election 2",
-          numberOfCandidates: 5,
-        },
-        {
-          id: 3,
-          title: "Election 3",
-          numberOfCandidates: 2,
-        },
-      ];
-      setVoters(data);
+      setVoters(listOfMockPolls);
       setIsLoading(false);
     }, 4000);
   }, []);
@@ -43,33 +21,34 @@ const VoterPage = () => {
   return (
     <div className={`flex-col bg-gradient-to-r from-[#181436] to-[#19244F] h-screen p-7`}>
       {voters && (
-        <div className="flex flex-row">
-          <div className="text-3xl font-bold mb-4">Poll Your Vote</div>
+        <div className="flex flex-row items-center mb-4">
+          <div className="text-3xl font-bold ">Active Polls</div>
+          <span className="h-3 w-3 animate-ping ml-3 inline-flex  rounded-full bg-green-400 opacity-75"></span>
         </div>
       )}
       {isLoading ? (
         <LoaderPage />
       ) : (
         voters?.map(voter => (
-          <div className="mb-4 mx-4" key={voter.id}>
+          <div className="mb-4 mx-4" key={voter.title}>
             <HoverBorderCard
               showArrow={true}
               click={() => {
                 // navigate to the vote page
-                router.push(`/vote?id=${voter.id}`);
+                router.push(`/vote?id=${voter.title}`);
               }}
             >
               <div className="flex flex-row ">
-                <img src="https://via.placeholder.com/150" alt="voter" className="w-14 h-14 mr-5 rounded-full" />
+                <img
+                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${voter.country.value}.svg`}
+                  alt="voter"
+                  className="w-14 h-14 mr-5 rounded-full border-2 shadow-xl "
+                />
 
                 <div className="flex flex-col">
                   <h1 className="text-lg font-bold">{voter.title}</h1>
-                  <h1 className="text-md text-slate-500">{voter.numberOfCandidates} Candidates</h1>
+                  <h1 className="text-md text-slate-500">{voter.options.length} Candidates</h1>
                 </div>
-                <span className="relative h-3 w-3  ml-4 flex self-center ">
-                  <div className=" badge badge-secondary badge-lg badge-outline px-6 ">Active</div>
-                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                </span>
               </div>
             </HoverBorderCard>
           </div>
