10a589bb8cf63caa1780c33c6ff8c3f4f60f5cb5
Manu <manu1998kj@gmail.com>
2024-03-16 08:56:01 +0000

    added lce state for home

--- packages/nextjs/components/loader/loader.tsx
+++ packages/nextjs/components/loader/loader.tsx
@@ -4,7 +4,16 @@ import Lottie from "lottie-react";
 import voteAnimation from "~~/components/assets/vote.json";
 
 const LoaderPage = () => {
-  return <Lottie animationData={voteAnimation} />;
+  const style = {
+    height: 400,
+    width: 400,
+  };
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <Lottie animationData={voteAnimation} style={style} />
+      <p className="text-center text-3xl font-mono font-extrabold">Loading...</p>
+    </div>
+  );
 };
 
 export default LoaderPage;
