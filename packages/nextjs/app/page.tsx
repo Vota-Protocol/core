0378d764c2424d45503425e8c48eb2f4db9e3e5c
Manu <manu1998kj@gmail.com>
2024-03-17 04:34:53 +0000

    added actie and inactive state

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -52,12 +52,12 @@ export default function VoterPage() {
     for (const poll of pollsRaw) {
       i++;
       if (!poll.result) continue;
-
       dataList.push({
         id: i + 1,
         title: (poll.result as any)[0] as string,
         options: decodeOptions((poll.result as any)[1] as `0x${string}`) as string[],
         country: { title: "India", value: "IN" },
+        expiry: Number((poll.result as any)[5]),
       });
     }
 
@@ -78,41 +78,77 @@ export default function VoterPage() {
   //     }, 4000);
   //   }
   // }, []);
+  const activePolls = pollsFormatted?.filter(poll => (poll?.expiry ?? 0) * 1000 > Date.now());
+  const inactivePolls = pollsFormatted?.filter(poll => (poll?.expiry ?? 0) * 1000 < Date.now());
   return (
     <div className={`flex-col h-full flex-1 p-5`}>
-      {pollsFormatted && (
-        <div className="flex flex-row items-center mb-7 ">
-          <div className="text-3xl font-bold ">Active Polls</div>
-          <span className="h-3 w-3 animate-ping ml-3 inline-flex  rounded-full bg-green-400 opacity-75"></span>
-        </div>
-      )}
+      {activePolls || inactivePolls ? (
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center my-7 ">
+            <div className="text-3xl font-bold ">Active Polls</div>
+            <span className="h-3 w-3 animate-ping ml-3 inline-flex  rounded-full bg-green-400 opacity-75"></span>
+          </div>
 
-      {pollsFormatted ? (
-        <div className="grid lg:grid-cols-2">
-          {pollsFormatted?.map(voter => (
-            <div className="mb-4 mx-2" key={voter.title}>
-              <HoverBorderCard
-                showArrow={true}
-                click={() => {
-                  // navigate to the vote page
-                  router.push(`/vote/${voter.id}`);
-                }}
-              >
-                <div className="flex flex-row ">
-                  <img
-                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${voter.country.value}.svg`}
-                    alt="voter"
-                    className="w-14 h-14 mr-5 rounded-full border-2 shadow-xl "
-                  />
+          <div className="grid lg:grid-cols-2">
+            {activePolls?.length == 0 && <div className="text-2xl text-gray-600 text-center">No Active Polls</div>}
+            {activePolls?.map(voter => (
+              <div className="my-4 mx-2" key={voter.title}>
+                <HoverBorderCard
+                  showArrow={true}
+                  click={() => {
+                    // navigate to the vote page
+                    router.push(`/vote/${voter.id}`);
+                  }}
+                >
+                  <div className="flex flex-row ">
+                    <img
+                      src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${voter.country.value}.svg`}
+                      alt="voter"
+                      className="w-14 h-14 mr-5 rounded-full border-2 shadow-xl "
+                    />
 
-                  <div className="flex flex-col">
-                    <h1 className="text-lg font-bold">{voter.title}</h1>
-                    <h1 className="text-md text-slate-500">{voter.options.length} Candidates</h1>
+                    <div className="flex flex-col">
+                      <h1 className="text-lg font-bold">{voter.title}</h1>
+                      <h1 className="text-md text-slate-500">{voter.options.length} Candidates</h1>
+                    </div>
                   </div>
-                </div>
-              </HoverBorderCard>
-            </div>
-          ))}
+                </HoverBorderCard>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex flex-row items-center my-7 ">
+            <div className="text-3xl font-bold ">InActive Polls</div>
+            <span className="h-3 w-3 animate-ping ml-3 inline-flex  rounded-full bg-red-400 opacity-75"></span>
+          </div>
+
+          <div className="grid lg:grid-cols-2">
+            {inactivePolls?.length == 0 && <div className="ext-2xl text-gray-600 text-center">No In-Active Polls</div>}
+            {inactivePolls?.map(voter => (
+              <div className="mb-4 mx-2" key={voter.title}>
+                <HoverBorderCard
+                  showArrow={true}
+                  click={() => {
+                    // navigate to the vote page
+                    router.push(`/vote/${voter.id}`);
+                  }}
+                >
+                  <div className="flex flex-row ">
+                    <img
+                      src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${voter.country.value}.svg`}
+                      alt="voter"
+                      className="w-14 h-14 mr-5 rounded-full border-2 shadow-xl "
+                    />
+
+                    <div className="flex flex-col">
+                      <h1 className="text-lg font-bold">{voter.title}</h1>
+                      <h1 className="text-md text-slate-500">{voter.options.length} Candidates</h1>
+                    </div>
+                  </div>
+                </HoverBorderCard>
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <LoaderPage message="Fetching Current Poll's .... " />
